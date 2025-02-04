ea1716e273d7df01297b449a6f438b4ded16711b
Keren Aguillon <aguillonkeren9@gmail.com>
2025-02-04 08:36:18 -0600

    añadir nombres

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ let listaNombres=[];
 function nombresIngresados(){
     let nombre = prompt('Ingresa un nombre: ')
 };
+
+function asignarTexto(nombres, texto){
+    const nombres = document.querySelector(".nombre");
+    nombresHTMl.innerHTML = texto;
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+/*if(listaNombres.includes(nombre)){
+    return nombresIngresados();
+}else{
+    listaNombres.push(nombre);
+    return nombresIngresados;
+}
+
+console.log(listaNombres);
+console.log(nombresIngresados);*/
